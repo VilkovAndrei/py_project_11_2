4cea92dff6e1db489a6badeef705d6fb8eb235cf
Andry Vilkov <vilkov-l-nn@mail.ru>
2023-11-30 22:34:09 +0300

    add str_func.py def _title_str()

--- str_func.py
+++ str_func.py
@@ -1,2 +1,15 @@
 def _upper_str(_str):
+    """
+    Возвращает строку в верхнем регистре
+    :param _str:
+    :return:
+    """
     return _str.upper()
+
+def _title_str(_str):
+    """
+    Переводит первые буквы слов в строке в верхний регистр
+    :param _str:
+    :return:
+    """
+    return _str.title()
